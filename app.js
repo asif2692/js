cc1fd3203896fec8a9b9227a9298878c20f5054c
asif2692 <asifraza26792@gmail.com>
2023-09-23 15:00:17 +0500

    js

---
+++ app.js
@@ -0,0 +1,15 @@
+document.write("<h2> Muhammad Asif  </h2>")
+
+alert("Wellcome to Js first Claas  Alert");
+console.log("This Example Off console")
+alert("this is line brack Example \n step 1 line1 \n step 2 line2 \n step 3 line3 \n step 4 line4")
+alert("My Email Address is asifraza26792@gmail.com")
+
+
+prompt("pleas Enter Your Ful Name")
+var Name = " CCO-117396"
+var Example2 = "this is Variable alart Example"
+
+
+document.write(Name)
+alert(Example2)
